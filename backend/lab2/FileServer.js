877849b1edcb8ca21549a934f77a30867b6c3fd9
arunesh-unoff-1111 <aruneshkr9@gmail.com>
2026-08-24 16:25:38 +0530

    today : createfile, readfile, appendfile, deletefile

---
+++ backend/lab2/FileServer.js
@@ -0,0 +1,50 @@
+// File Creating , Reading , Writing , Delting , Updating
+import fs from "node:fs/promises";
+const filePath = "userData.txt";
+
+async function createFile(content) {
+  try {
+    await fs.writeFile(filePath, content, "utf8");
+    console.log("File created successfully!");
+  } catch (err) {
+    console.error("Error creating file:", err);
+  }
+}
+
+async function readFile() {
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    console.log("File Content:\n", data);
+  } catch (err) {
+    console.error("Error reading file:", err);
+  }
+}
+
+
+async function appendFile(content) {
+  try {
+    await fs.appendFile(filePath, content, "utf8");
+    console.log("File updated successfully!");
+  } catch (err) {
+    console.error("Error updating file:", err);
+  }
+}
+
+
+async function deleteFile() {
+  try {
+    await fs.unlink(filePath);
+    console.log("File deleted successfully!");
+  } catch (err) {
+    console.error("Error deleting file:", err);
+  }
+}
+
+async function runDemo() {
+//   await createFile("This file is created form FileServer.js using JS codes !!! Hurrey ! \n");
+  await appendFile("Hey everyone I am your Boss !!! Hahahaha...\n");
+  await readFile();
+//   await deleteFile();
+}
+
+runDemo();
